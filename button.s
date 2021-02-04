5419aefd16bbb35858bc1057ddd80e549e6ee0c9
Zhuochen-Wu <70975240+Zhuochen-Wu@users.noreply.github.com>
2021-02-04 11:02:15 -0500

    Update button.s

--- button.s
+++ button.s
@@ -14,9 +14,9 @@
 ;Define your I/O Port Addresses Here
 
 SYSCTL_RCGCGPIO_R             EQU     0x400FE608         ;General-Purpose Input/Output Run Mode Clock Gating Control Register
-GPIO_PORTN_DIR_R              EQU     0x40064400         ;GPIO Port N DIR Register
-GPIO_PORTN_DEN_R              EQU     0x4006451C         ;GPIO Port N DEN Register
-GPIO_PORTN_DATA_R             EQU     0x400643FC         ;GPIO Port N DATA Register 
+GPIO_PORTF_DIR_R              EQU     0x4005D400         ;GPIO Port N DIR Register
+GPIO_PORTF_DEN_R              EQU     0x4005D51C         ;GPIO Port N DEN Register
+GPIO_PORTF_DATA_R             EQU     0x4005D3FC         ;GPIO Port N DATA Register 
 GPIO_PORTM_DIR_R              EQU     0x40063400         ;GPIO Port N DIR Register
 GPIO_PORTM_DEN_R              EQU     0x4006351C         ;GPIO Port N DEN Register
 GPIO_PORTM_DATA_R             EQU     0x400633FC         ;GPIO Port N DATA Register 
@@ -27,19 +27,19 @@ GPIO_PORTM_DATA_R             EQU     0x400633FC         ;GPIO Port N DATA Regis
         EXPORT Start
 
 ;Function PortN_Init
-PortNM_Init 
+PortFM_Init 
 		;STEP 1
 		 LDR R1, =SYSCTL_RCGCGPIO_R    		; Load the memory address of RCGCGPIO into R1
 		 LDR R0, [R1]						; Load the contents of the memory address of RCGCGPIO into R0
-		 ORR R0,R0, #0x1800					; Perform a bitwise OR operation with contents of R0 and 0x1800 and stores it in R0
+		 ORR R0,R0, #0x820					; Perform a bitwise OR operation with contents of R0 and 0x1800 and stores it in R0
 		 STR R0, [R1]						; Store what is in R0 into R1
 		 NOP
 		 NOP
 		
 		;STEP 5
-		 LDR R1, =GPIO_PORTN_DIR_R			; Load the memory address of the GPIO Port N DIR Register into R1
+		 LDR R1, =GPIO_PORTF_DIR_R			; Load the memory address of the GPIO Port N DIR Register into R1
 		 LDR R0, [R1]						; Load the contents of the memory address of GPIO Port N DIR Register in R0
-		 ORR R0,R0, #0x1					; Perform a bitwise OR operation with the contents of R0 with 0x2 and store it back into R0
+		 ORR R0,R0, #0x10					; Perform a bitwise OR operation with the contents of R0 with 0x2 and store it back into R0
 		 STR R0, [R1]						; Store what is in R0 into R1
 		 
 		 LDR R1, =GPIO_PORTM_DIR_R			; Load the memory address of the GPIO Port N DIR Register into R1
@@ -48,9 +48,9 @@ PortNM_Init
 		 STR R0, [R1]						; Store what is in R0 into R1
 		 
 		;STEP 7
-		 LDR R1, =GPIO_PORTN_DEN_R			; Load the memory address of the GPIO Port N DEN Register into R1
+		 LDR R1, =GPIO_PORTF_DEN_R			; Load the memory address of the GPIO Port N DEN Register into R1
 		 LDR R0, [R1]						; Load the contents of the memory address of GPIO Port N DEN Register in R0
-		 ORR R0,R0, #0x1					; Perform a bitwise OR operation with the contents of R0 with 0x2 and store it back into R0
+		 ORR R0,R0, #0x10					; Perform a bitwise OR operation with the contents of R0 with 0x2 and store it back into R0
 		 STR R0, [R1]						; Store what is in R0 into R1
 		 
 		 LDR R1, =GPIO_PORTM_DEN_R			; Load the memory address of the GPIO Port N DEN Register into R1
@@ -61,22 +61,22 @@ PortNM_Init
         BX LR               ; return from function 
        
 Start 
-	    BL PortNM_Init       ; calls and execute your PortN_Init function
+	    BL PortFM_Init       ; calls and execute your PortN_Init function
 		;STEP 8    
 		
-		 LDR R1, =GPIO_PORTN_DATA_R			; Load the memory address of the GPIO Port N DATA Register into R1
+		 LDR R1, =GPIO_PORTF_DATA_R			; Load the memory address of the GPIO Port N DATA Register into R1
 		 LDR R0, [R1]						; Load the contents of the memory address of GPIO Port N DATA Register in R0
 
-blink	 LDR R2, =GPIO_PORTM_DATA_R			; Load the memory address of the GPIO Port N DATA Register into R2
+loop	 LDR R2, =GPIO_PORTM_DATA_R			; Load the memory address of the GPIO Port N DATA Register into R2
 		 LDR R3, [R2]						; Load the contents of R2 into R3
 		 
 		 CMP R3, #0x0						; Compare R3 with 0
 		 ITE EQ								; if R3 is 0, then it means that LED is on, otherwisw on
-		 ORREQ R0,R0, #0x1
+		 ORREQ R0,R0, #0x10
 		 ANDNE R0,R0, #0x0
 		 
 		 STR R0, [R1]
-		B blink
+		B loop
 		
 		ALIGN               ; directive for assembly			
         END                 ; End of function 
